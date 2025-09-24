5729150c0eef57384bf2a467a3f0ac30d204a2a9
Tuomas Pöyry <me@tuomas.id>
2025-09-24 18:29:57 +0300

    Switch back to default Vite port

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: parseInt(process.env.PORT || '8080'),
-    allowedHosts: ['.diploi.me'],
-  },
   plugins: [react()],
 });
